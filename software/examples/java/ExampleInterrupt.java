1539ab29d4ce596e743681fff5f9b5d090968472
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 14:27:20 +0100

    Make Java examples Protocol 2.0 compatible

--- software/examples/java/ExampleInterrupt.java
+++ software/examples/java/ExampleInterrupt.java
@@ -7,15 +7,13 @@ public class ExampleInterrupt {
 	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-	//       might normally want to catch are described in the commnents below
+	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
-		// Create connection to brickd
-		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
-		BrickletIO4 io4 = new BrickletIO4(UID); // Create device object
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletIO4 io4 = new BrickletIO4(UID, ipcon); // Create device object
 
-		// Add device to IP connection
-		ipcon.addDevice(io4); // Can throw IPConnection.TimeoutException
-		// Don't use device before it is added to a connection
+		ipcon.connect(host, port); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Add and implement listener for interrupt (called if pin 0 changes)
 		io4.addListener(new BrickletIO4.InterruptListener() {
@@ -29,6 +27,5 @@ public class ExampleInterrupt {
 		io4.setInterrupt((short)(1 << 0));
 
 		System.console().readLine("Press key to exit\n");
-		ipcon.destroy();
 	}
 }
--- software/examples/java/ExampleOutput.java
+++ software/examples/java/ExampleOutput.java
@@ -7,15 +7,13 @@ public class ExampleOutput {
 	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-	//       might normally want to catch are described in the commnents below
+	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
-		// Create connection to brickd
-		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
-		BrickletIO4 io4 = new BrickletIO4(UID); // Create device object
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletIO4 io4 = new BrickletIO4(UID, ipcon); // Create device object
 
-		// Add device to IP connection
-		ipcon.addDevice(io4); // Can throw IPConnection.TimeoutException
-		// Don't use device before it is added to a connection
+		ipcon.connect(host, port); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set pin 1 output low
 		io4.setConfiguration((short)(1 << 1), 'o', false);
@@ -24,6 +22,5 @@ public class ExampleOutput {
 		io4.setConfiguration((short)((1 << 2) | (1 << 3)), 'o', true);
 
 		System.console().readLine("Press key to exit\n");
-		ipcon.destroy();
 	}
 }
