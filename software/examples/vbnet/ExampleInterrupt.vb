b63c81a5eab4a07521e3d3e637bc8a8aee4cacb5
Matthias Bolte <matthias@tinkerforge.com>
2013-02-18 16:29:21 +0100

    Add Visual Basic .NET examples

---
+++ software/examples/vbnet/ExampleInterrupt.vb
@@ -0,0 +1,35 @@
+Imports Tinkerforge
+
+Module ExampleInterrupt
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "adL" ' Change to your UID
+
+    ' Callback function for interrupts
+    Sub InterruptCB(ByVal sender As BrickletIO4, _
+                    ByVal interruptMask As Byte, ByVal valueMask As Byte)
+        Dim interruptBinary As String = System.Convert.ToString(interruptMask, 2)
+        Dim valueBinary As String = System.Convert.ToString(valueMask, 2)
+
+        System.Console.WriteLine("Interrupt by: " + interruptBinary)
+        System.Console.WriteLine("Value: " + valueBinary)
+    End Sub
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim io4 As New BrickletIO4(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Register callback for interrupts
+        AddHandler io4.Interrupt, AddressOf InterruptCB
+
+        ' Enable interrupt on pin 0
+        io4.SetInterrupt(1 << 0)
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadKey()
+        ipcon.Disconnect()
+    End Sub
+End Module
---
+++ software/examples/vbnet/ExampleOutput.vb
@@ -0,0 +1,25 @@
+Imports Tinkerforge
+
+Module ExampleSimple
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "adL" ' Change to your UID
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim io4 As New BrickletIO4(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Set pin 1 to output low
+        io4.SetConfiguration(1 << 1, "o"C, false)
+
+        ' Set pin 2 and 3 to output high
+        io4.SetConfiguration((1 << 2) or (1 << 3), "o"C, true)
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadKey()
+        ipcon.Disconnect()
+    End Sub
+End Module
