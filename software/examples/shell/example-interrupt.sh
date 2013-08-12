233d24ed8cde25d75dd107518e5597a062c98ccb
Matthias Bolte <matthias@tinkerforge.com>
2013-08-12 16:50:42 +0200

    Add shell examples

---
+++ software/examples/shell/example-interrupt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# connects to localhost:4223 by default, use --host and --port to change it
+
+# change to your UID
+uid=XYZ
+
+# enable interrupt on pin 0
+tinkerforge call io4-bricklet $uid set-interrupt 1
+
+# handle incoming interrupt callbacks
+tinkerforge dispatch io4-bricklet $uid interrupt
---
+++ software/examples/shell/example-output.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# connects to localhost:4223 by default, use --host and --port to change it
+
+# change to your UID
+uid=XYZ
+
+# get current illuminance (unit is Lux/10)
+tinkerforge call ambient-light-bricklet $uid get-illuminance
+
+
+# set pin 1 to output low: (1 << 1) = 2
+tinkerforge call io4-bricklet $uid set-configuration 2 output false
+
+# set pin 2 and 3 to output high: (1 << 2) | (1 << 3) = 12
+tinkerforge call io4-bricklet $uid set-configuration 12 output true
