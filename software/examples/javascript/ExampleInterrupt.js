9fe502738de812129e14f3adc5206d221818b0b3
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-12 12:37:23 +0100

    javascript: add examples

---
+++ software/examples/javascript/ExampleInterrupt.js
@@ -0,0 +1,42 @@
+var IPConnection = require('Tinkerforge/IPConnection');
+var BrickletIO4 = require('Tinkerforge/BrickletIO4');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'gVq';// Change to your UID
+
+var ipcon = new IPConnection();// Create IP connection
+var io = new BrickletIO4(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        console.log('Error: '+error);        
+    }
+);// Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        //Enable interrupt on pin 0
+        io.setInterrupt(1 << 0);
+    }
+);
+
+// Register callback for interrupts
+io.on(BrickletIO4.CALLBACK_INTERRUPT,
+    // Callback function for interrupts
+    function(interruptMask, valueMask) {
+        console.log('Interrupt by: ' + parseInt(interruptMask).toString(2));
+        console.log('Value: ' + parseInt(valueMask).toString(2));
+        console.log();
+    }    
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
+
---
+++ software/examples/javascript/ExampleOutput.js
@@ -0,0 +1,34 @@
+var IPConnection = require('Tinkerforge/IPConnection');
+var BrickletIO4 = require('Tinkerforge/BrickletIO4');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'gVq';// Change to your UID
+
+var ipcon = new IPConnection();// Create IP connection
+var io = new BrickletIO4(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        console.log('Error: '+error);        
+    }
+);// Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Set pin 1 to output low
+        io.setConfiguration(1 << 1, 'o', false);
+        // Set pin 2 and 3 to output high
+        io.setConfiguration((1 << 2) | (1 << 3), 'o', true);
+       }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
+
