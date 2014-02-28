141ab974b152e20101eba25f819dc16c46231aca
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:18:35 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleInterrupt.js
+++ software/examples/javascript/ExampleInterrupt.js
@@ -2,21 +2,21 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'gVq';// Change to your UID
+var UID = 'gVq'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var io = new Tinkerforge.BrickletIO4(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var io = new Tinkerforge.BrickletIO4(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
-        //Enable interrupt on pin 0
+        // Enable interrupt on pin 0
         io.setInterrupt(1 << 0);
     }
 );
@@ -28,7 +28,7 @@ io.on(Tinkerforge.BrickletIO4.CALLBACK_INTERRUPT,
         console.log('Interrupt by: ' + parseInt(interruptMask).toString(2));
         console.log('Value: ' + parseInt(valueMask).toString(2));
         console.log();
-    }    
+    }
 );
 
 console.log("Press any key to exit ...");
@@ -38,4 +38,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
--- software/examples/javascript/ExampleOutput.js
+++ software/examples/javascript/ExampleOutput.js
@@ -2,25 +2,25 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'gVq';// Change to your UID
+var UID = 'gVq'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var io = new Tinkerforge.BrickletIO4(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var io = new Tinkerforge.BrickletIO4(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set pin 1 to output low
         io.setConfiguration(1 << 1, 'o', false);
         // Set pin 2 and 3 to output high
         io.setConfiguration((1 << 2) | (1 << 3), 'o', true);
-       }
+    }
 );
 
 console.log("Press any key to exit ...");
@@ -30,4 +30,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
