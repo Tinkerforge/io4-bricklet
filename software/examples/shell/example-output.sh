00ac98bf2182d090995483311d1e0e737dc61007
Matthias Bolte <matthias@tinkerforge.com>
2017-04-20 19:32:36 +0200

    shell: Adapt example

--- software/examples/shell/example-output.sh
+++ software/examples/shell/example-output.sh
@@ -4,7 +4,7 @@
 uid=XYZ # Change XYZ to the UID of your IO-4 Bricklet
 
 # Set pin 1 to output low: 1 << 1 = 2
-tinkerforge call io4-bricklet $uid set-configuration 2 out false
+tinkerforge call io4-bricklet $uid set-configuration 2 direction-out false
 
 # Set pin 2 and 3 to output high: (1 << 2) | (1 << 3) = 12
-tinkerforge call io4-bricklet $uid set-configuration 12 out true
+tinkerforge call io4-bricklet $uid set-configuration 12 direction-out true
