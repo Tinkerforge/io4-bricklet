2751426e421207676febcbc3eb2090f1939994da
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:27:10 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_interrupt.rs
+++ software/examples/rust/example_interrupt.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for interrupt events.
-    let interrupt_receiver = io.get_interrupt_receiver();
+    let interrupt_receiver = io.get_interrupt_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `io` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `io` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for interrupt in interrupt_receiver {
